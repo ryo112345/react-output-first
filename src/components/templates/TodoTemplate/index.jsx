5380cb814eefafcd1cd7a4a9112d1efaddd1320c
ryo112345 <ryo112345swe@gmail.com>
2025-08-10 11:18:42 +0900

    feat: データ構造をオブジェクト配列に変更とdata.js作成
      変更ファイル:
      src/constants/data.js
      - INIT_TODO_LIST配列を作成（id, title構造）
      - INIT_UNIQUE_ID定数を追加
    
      src/components/templates/TodoTemplate/index.jsx
      - data.jsからINIT_TODO_LISTPをインポート
      - 固定配列[Todo1, Todo2]からオブジェクト配列に変更
      - map()でtodo.title表示、key={todo.id}に変更
    
      結果: npm run dev → サンプルコードと同じデータ構造でTodo表示

--- src/components/templates/TodoTemplate/index.jsx
+++ src/components/templates/TodoTemplate/index.jsx
@@ -1,9 +1,12 @@
+  import { INIT_TODO_LIST } from "../../../constants/data.js";
+
   export const TodoTemplate = () => {
     return (
       <div>
         <h1>Todo List</h1>
-        <p>Todo1</p>
-        <p>Todo2</p>
+        {INIT_TODO_LIST.map((todo) => (
+          <p key={todo.id}>{todo.title}</p>
+        ))}
       </div>
     );
   };
